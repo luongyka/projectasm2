4bb17cf628ea29ae5e53f93124e0490dfa881c98
Duc Luong <luongyka@gmail.com>
2020-12-26 10:56:59 +0700

    u

--- php/database.php
+++ php/database.php
@@ -1,10 +1,10 @@
 <?php
 
-define('PG_HOST', 'ec2-34-200-15-192.compute-1.amazonaws.com');
+define('PG_HOST', 'ec2-54-84-98-18.compute-1.amazonaws.com');
 define('PG_PORT', 5432);
-define('PG_DATABASE', 'dbm9tm52lqr0bo');
-define('PG_USER', 'emlsbprybpkbkc');
-define('PG_PASSWORD', '5be64aebe8b8fcd3f08403749abf3979914cc85b63c82d7cfed5b89b0b1e9d93');
+define('PG_DATABASE', 'd89sq7n25h7h3q');
+define('PG_USER', 'ezjvhzsvkbjfoy');
+define('PG_PASSWORD', '5ae06b5871a7e0039761da957fbfa71c54f760c2c83a0f6e513d3f5d9c851bb5');
 define('ERROR_ON_CONNECT_FAILED', 'Connection failed!');
 
 function getDB() {
